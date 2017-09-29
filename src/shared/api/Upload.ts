e88f67d438f3a6e299e813cace04d043af7b7c86
吴学谦 <wuxueqian@ezbuy.com>
2017-09-29 17:38:41 +0800

    👨‍👩‍👦‍👦 correct upload api

--- src/shared/api/Upload.ts
+++ src/shared/api/Upload.ts
@@ -2,11 +2,11 @@ import CommonResp from "model/Resp";
 import WebApi from "api/WebApi";
 
 export function UploadImage(fileData: FormData) {
-    return WebApi.Put<CommonResp<string>>("upload/images", fileData);
+    return WebApi.Post<CommonResp<string>>("upload/images", fileData);
 }
 
 export function UploadAvatar(fileData: FormData) {
-    return WebApi.Put<CommonResp<string>>("upload/avatars", fileData);
+    return WebApi.Post<CommonResp<string>>("upload/avatars", fileData);
 }
 
 export default {
