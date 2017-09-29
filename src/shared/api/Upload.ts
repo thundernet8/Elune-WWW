6d82c41b9f975ced5f2e0e099b2b3eb345d86554
wuxueqian <wuxueqian2010@hotmail.com>
2017-09-29 20:45:40 +0800

    👳 fix travis build error

--- src/shared/api/Upload.ts
+++ src/shared/api/Upload.ts
@@ -2,7 +2,7 @@ import CommonResp from "model/Resp";
 import WebApi from "api/WebApi";
 
 export function UploadImage(fileData: FormData) {
-    return WebApi.Post<CommonResp<string>>("upload/images", fileData);
+    return WebApi.Post<CommonResp<string[]>>("upload/images", fileData);
 }
 
 export function UploadAvatar(fileData: FormData) {
