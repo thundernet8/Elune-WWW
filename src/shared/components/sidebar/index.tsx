9e61b5bd1721d6e417b8a62d4fec3239ef9632b8
thundernet8 <wuxueqian2010@hotmail.com>
2019-03-30 22:26:10 +0800

    fix api

--- src/shared/components/sidebar/index.tsx
+++ src/shared/components/sidebar/index.tsx
@@ -32,7 +32,7 @@ export default class Sidebar extends React.Component<
                     <div className="widget-content">
                         <a href="https://webapproach.net/go/do" target="_blank">
                             <img
-                                src="https://elune.fuli.news/content/images/2017/10/19/a127058b1f84e24760e7da360978a746.png"
+                                src="https://api.elune.me/content/images/2017/10/19/a127058b1f84e24760e7da360978a746.png"
                                 style={{ maxWidth: "100%" }}
                             />
                         </a>
@@ -42,7 +42,7 @@ export default class Sidebar extends React.Component<
                     <div className="widget-content">
                         <a href="https://webapproach.net/go/cl" target="_blank">
                             <img
-                                src="https://elune.fuli.news/content/images/2017/10/19/b1f7279c640a51c2d4bf5983a328ad64.jpg"
+                                src="https://api.elune.me/content/images/2017/10/19/b1f7279c640a51c2d4bf5983a328ad64.jpg"
                                 style={{ maxWidth: "100%" }}
                             />
                         </a>
