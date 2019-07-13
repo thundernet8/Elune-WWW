ed6b50392bae1780653d12d8f8874f3d9d3e5c91
wuxueqian <wuxueqian2010@hotmail.com>
2019-07-13 17:27:49 +0800

    updates

--- webpack/dll.conf.babel.js
+++ webpack/dll.conf.babel.js
@@ -1,18 +1,18 @@
-import path from "path";
-import webpack from "webpack";
-const AssetsPlugin = require("assets-webpack-plugin");
+import path from 'path';
+import webpack from 'webpack';
+const AssetsPlugin = require('assets-webpack-plugin');
 
-const isDev = process.env.NODE_ENV !== "production";
+const isDev = process.env.NODE_ENV !== 'production';
 const getPlugins = function() {
     let plugins = [
         new webpack.DllPlugin({
             context: __dirname,
-            path: "manifest.json",
-            name: "[name]_[chunkhash:8]"
+            path: 'manifest.json',
+            name: '[name]_[chunkhash:8]'
         }),
         new AssetsPlugin({
-            filename: "venders-config.json",
-            path: "./"
+            filename: 'venders-config.json',
+            path: './'
         }),
         new webpack.HashedModuleIdsPlugin()
     ];
@@ -33,40 +33,40 @@ const getPlugins = function() {
 const config = {
     entry: {
         venders: [
-            "react",
-            "react-dom",
-            "react-router",
-            "react-router-dom",
-            "babel-polyfill",
-            "mobx",
-            "mobx-react",
-            "axios",
-            "classnames",
-            "react-headroom",
-            "react-document-meta",
+            'react',
+            'react-dom',
+            'react-router',
+            'react-router-dom',
+            'babel-polyfill',
+            'mobx',
+            'mobx-react',
+            'axios',
+            'classnames',
+            'react-headroom',
+            'react-document-meta',
             // "element-react",
-            "element-react/next",
-            "draft-js",
-            "react-draft-wysiwyg",
-            "moment"
+            'element-react/next',
+            'draft-js',
+            'react-draft-wysiwyg',
+            'moment'
         ]
     },
     output: {
-        path: path.resolve(__dirname, "../dist/assets/js"),
+        path: path.resolve(__dirname, '../dist/assets/js'),
         publicPath: isDev
-            ? "/assets/js/"
-            : "https://s0.parser.cc/elune/assets/js/",
-        filename: "[name].[chunkhash:8].js",
-        library: "[name]_[chunkhash:8]"
+            ? '/assets/js/'
+            : 'https://static-0-1254131461.cos.ap-shanghai.myqcloud.com/elune/assets/js/',
+        filename: '[name].[chunkhash:8].js',
+        library: '[name]_[chunkhash:8]'
     },
     resolve: {
-        modules: ["node_modules", path.resolve(__dirname, "../src/shared")]
+        modules: ['node_modules', path.resolve(__dirname, '../src/shared')]
     },
     plugins: getPlugins()
 };
 
 if (isDev) {
-    config.devtool = "#source-map"; // '#eval-source-map'
+    config.devtool = '#source-map'; // '#eval-source-map'
 }
 
 export default config;
